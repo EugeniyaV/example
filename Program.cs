f225f050a65d587c1140c213791f9d50b115cc67
Eugeniya Voroshilo <j_v.k@mail.ru>
2015-09-26 09:45:47 +0300

    Ex_ch2

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConfigureGUI();
+            
+            Console.WriteLine();
+        }
+
+        private static void ConfigureGUI()
+        {
+            Console.Title = "My Rocking App";
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine("***********************************");
+            Console.WriteLine("**** Welcome to My Rocking App ****");
+            Console.WriteLine("***********************************");
+            Console.BackgroundColor = ConsoleColor.Black;
+        }
+    }
+}
---
+++ SpCar.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ch2
+{
+    public class SpCar : Car
+    {
+        public string GetPetName()
+        {
+            petName = "Fred";
+            return petName;
+        }
+    }
+}
